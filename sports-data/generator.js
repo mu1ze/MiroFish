5375abc66a4c9fab8d35d73eb4ba0d792e29aaa0
mu1ze <146881855+mu1ze@users.noreply.github.com>
2026-03-18 15:37:25 -0400

    Add generator.js

---
+++ sports-data/generator.js
@@ -0,0 +1,19 @@
+🏀 MiroFish Sports Data Generator
+===================================
+
+Usage: node generator.js "<player-name>" [games]
+
+Examples:
+  node generator.js "jaylen brown" 20
+  node generator.js "jayson tatum" 10
+  node generator.js "stephen curry" 15
+
+Available players:
+  - jaylen brown
+  - jayson tatum  
+  - stephen curry
+  - lebron james
+  - luka doncic
+
+Note: This generates the report template. 
+The actual data comes from the web search results we got earlier.
